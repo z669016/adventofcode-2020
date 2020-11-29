4920c5a89a4e69e066d00a959c131841162ecb9b
er21jq <rene.van.bpi.putten@ing.com>
2020-11-29 11:10:49 +0100

    cleanup

--- src/main/java/com/putoet/day1/Day1.java
+++ src/main/java/com/putoet/day1/Day1.java
@@ -1,13 +1,5 @@
 package com.putoet.day1;
 
-import com.putoet.resources.ResourceLines;
-
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 public class Day1 {
     public static void main(String[] args) {
     }

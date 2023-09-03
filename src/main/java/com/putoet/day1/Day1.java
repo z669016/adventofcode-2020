a30667e58b41f096901baf3cc7589157f80c70ed
Rene van Putten <z669016@gmail.com>
2023-09-03 11:34:34 +0300

    Introduced, Timer, var and minor code optimizations.

--- src/main/java/com/putoet/day1/Day1.java
+++ src/main/java/com/putoet/day1/Day1.java
@@ -1,46 +1,39 @@
 package com.putoet.day1;
 
 import com.putoet.resources.ResourceLines;
+import com.putoet.utils.Timer;
 
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class Day1 {
     public static void main(String[] args) {
-        part1();
-        part2();
+        final var values = new HashSet<>(ResourceLines.list("/day1.txt", Integer::parseInt));
+
+        Timer.run(() -> part1(values));
+        Timer.run(() -> part2(values));
     }
 
-    private static void part1() {
-        final Set<Integer> values = ResourceLines.list("/day1.txt").stream()
-                .map(Integer::valueOf)
-                .collect(Collectors.toSet());
-
-        for (Integer first : values) {
+    private static void part1(Set<Integer> values) {
+        for (var first : values) {
             if (values.contains(2020 - first)) {
-                final int a = first;
-                final int b = 2020 - first;
-                System.out.printf("Values are %d and %d, multiplying to %d%n", a, b, a * b);
+                final var b = 2020 - first;
+                System.out.printf("Values are %d and %d, multiplying to %d%n", first, b, first * b);
                 return;
             }
         }
     }
 
-    private static void part2() {
-        final Set<Integer> values = ResourceLines.list("/day1.txt").stream()
-                .map(Integer::valueOf)
-                .collect(Collectors.toSet());
-
-        for (Integer first : values) {
-            for (Integer second : values) {
-                if (second == first) continue;
-                if ((first == 2020 - first - second) || (second == 2020 - first - second)) continue;;
+    private static void part2(Set<Integer> values) {
+        for (var first : values) {
+            for (var second : values) {
+                if (Objects.equals(second, first)) continue;
+                if ((first == 2020 - first - second) || (second == 2020 - first - second)) continue;
 
                 if (values.contains(2020 - first - second)) {
-                    final int a = first;
-                    final int b = second;
-                    final int c = 2020 - first - second;
-                    System.out.printf("Values are %d and %d and %d, multiplying to %d%n", a, b, c, a * b * c);
+                    final var c = 2020 - first - second;
+                    System.out.printf("Values are %d and %d and %d, multiplying to %d%n", first, second, c, first * second * c);
                     return;
                 }
             }

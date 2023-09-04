027684784d318bba8974996f90173c0fae21087d
Rene van Putten <z669016@gmail.com>
2023-09-04 16:47:53 +0300

    Introduced, Timer, var, reduced scope where possible and minor code optimizations.

--- src/main/java/com/putoet/day13/Day13.java
+++ src/main/java/com/putoet/day13/Day13.java
@@ -1,59 +1,55 @@
 package com.putoet.day13;
 
 import com.putoet.resources.ResourceLines;
+import com.putoet.utils.Timer;
 import org.apache.commons.math3.util.ArithmeticUtils;
 import org.javatuples.Pair;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Day13 {
-    private static List<Pair<Integer, Integer>> delays;
-
     public static void main(String[] args) {
-        final List<String> puzzleInput = ResourceLines.list(("/day13.txt"));
-        final int departureTime = Integer.parseInt(puzzleInput.get(0));
+        final var puzzleInput = ResourceLines.list(("/day13.txt"));
+        final var departureTime = Long.parseLong(puzzleInput.get(0));
 
-        part1(departureTime, puzzleInput);
+        Timer.run(() -> part1(departureTime, puzzleInput));
 
-        final List<String> busses = Arrays.stream(puzzleInput.get(1).split(",")).collect(Collectors.toList());
-        part2(busses);
+        final var busses = Arrays.stream(puzzleInput.get(1).split(",")).toList();
+        Timer.run(() -> part2(busses));
     }
 
-    static void part1(int departureTime, List<String> puzzleInput) {
-        final Pair<Integer, Integer> bus = firstDeparture(departureTime, puzzleInput);
-        System.out.println("First bus to leave is " + bus.getValue0() + " in " + bus.getValue1() + " minutes");
+    static void part1(long departureTime, @NotNull List<String> puzzleInput) {
+        final var bus = firstDeparture(departureTime, puzzleInput);
         System.out.println("The ID of the earliest bus you can take to the airport multiplied by the number of minutes you'll need to wait for that bus is " + bus.getValue0() * bus.getValue1());
     }
 
-    static void part2(List<String> busses) {
-        final List<Pair<Integer, Integer>> delays = delays(busses);
+    static void part2(@NotNull List<String> busses) {
+        final var delays = delays(busses);
         System.out.println("First matching timestamp is " + findFirstMatchingTimestamp(delays));
     }
 
-    static long findFirstMatchingTimestamp(@NotNull List<Pair<Integer, Integer>> delays) {
-        Day13.delays = delays;
+    static long findFirstMatchingTimestamp(@NotNull List<Pair<Long,Long>> delays) {
         assert !delays.isEmpty();
 
-        final long maxTimestamp = IntStream.range(1, delays.size())
-                .mapToLong(i -> (long) delays.get(i).getValue0())
+        final var maxTimestamp = IntStream.range(1, delays.size())
+                .mapToLong(i -> delays.get(i).getValue0())
                 .reduce(delays.get(0).getValue0(), ArithmeticUtils::lcm);
 
-        final boolean[] matched = new boolean[delays.size()];
-        final Pair<Integer,Integer> max = delays.stream().max(Comparator.comparingLong(Pair::getValue0)).orElseThrow();
+        final var matched = new boolean[delays.size()];
+        final var max = delays.stream().max(Comparator.comparingLong(Pair::getValue0)).orElseThrow();
 
-        long step = max.getValue0();
-        long timestamp = step - max.getValue1();
-        boolean allMatches = false;
+        var step = max.getValue0();
+        var timestamp = step - max.getValue1();
+        var allMatches = false;
 
         while (!allMatches && timestamp <= maxTimestamp) {
-            boolean adjusted = false;
+            var adjusted = false;
 
             allMatches = true;
-            for (int idx = 0; idx < delays.size(); idx++) {
-                final Pair<Integer, Integer> pair = delays.get(idx);
+            for (var idx = 0; idx < delays.size(); idx++) {
+                final var pair = delays.get(idx);
                 if (!matched[idx]) {
                     matched[idx] = (timestamp + pair.getValue1()) % pair.getValue0() == 0;
                     if (matched[idx]) {
@@ -70,26 +66,26 @@ public class Day13 {
         return timestamp;
     }
 
-    public static Pair<Integer, Integer> firstDeparture(int departureTime, List<String> puzzleInput) {
+    static Pair<Integer, Integer> firstDeparture(long departureTime, @NotNull List<String> puzzleInput) {
         return Arrays.stream(puzzleInput.get(1).split(","))
                 .filter(bus -> !"x".equals(bus))
                 .mapToInt(Integer::parseInt)
-                .mapToObj(bus -> new Pair<>(bus, waitingTime(departureTime, bus)))
+                .mapToObj(bus -> Pair.with(bus, waitingTime(departureTime, bus)))
                 .min(Comparator.comparing(Pair::getValue1))
                 .orElseThrow(NoSuchElementException::new);
     }
 
     private static int waitingTime(long timestamp, int bus) {
-        final int nextArrival = (int) timestamp % bus;
+        final var nextArrival = (int) timestamp % bus;
         return nextArrival == 0 ? nextArrival : bus - nextArrival;
     }
 
-    public static List<Pair<Integer, Integer>> delays(List<String> busses) {
-        final List<Pair<Integer, Integer>> delays = new ArrayList<>();
-        for (int idx = 0; idx < busses.size(); idx++) {
-            final String bus = busses.get(idx);
+    static List<Pair<Long,Long>> delays(@NotNull List<String> busses) {
+        final var delays = new ArrayList<Pair<Long,Long>>();
+        for (var idx = 0; idx < busses.size(); idx++) {
+            final var bus = busses.get(idx);
             if (!"x".equals(bus)) {
-                delays.add(new Pair<>(Integer.parseInt(bus), idx));
+                delays.add(Pair.with(Long.parseLong(bus), (long) idx));
             }
         }
 
--- src/test/java/com/putoet/day13/Day13Test.java
+++ src/test/java/com/putoet/day13/Day13Test.java
@@ -15,9 +15,9 @@ class Day13Test {
 
     @Test
     void firstDeparture() {
-        final List<String> puzzleInput = ResourceLines.list(("/day13.txt"));
-        final int departureTime = Integer.parseInt(puzzleInput.get(0));
-        final Pair<Integer,Integer> bus = Day13.firstDeparture(departureTime, puzzleInput);
+        final var puzzleInput = ResourceLines.list(("/day13.txt"));
+        final var departureTime = Integer.parseInt(puzzleInput.get(0));
+        final var bus = Day13.firstDeparture(departureTime, puzzleInput);
 
         assertEquals(59, bus.getValue0());
         assertEquals(5, bus.getValue1());
@@ -25,16 +25,16 @@ class Day13Test {
 
     @Test
     void delays() {
-        final List<String> puzzleInput = ResourceLines.list(("/day13.txt"));
-        final List<String> busses = Arrays.stream(puzzleInput.get(1).split(",")).collect(Collectors.toList());
-        final List<Pair<Integer,Integer>> delays = Day13.delays(busses);
+        final var puzzleInput = ResourceLines.list(("/day13.txt"));
+        final var busses = Arrays.stream(puzzleInput.get(1).split(",")).collect(Collectors.toList());
+        final var delays = Day13.delays(busses);
 
-        final List<Pair<Integer,Integer>> answers = List.of(
-                new Pair<>(7, 0),
-                new Pair<>(13,1),
-                new Pair<>(59,4),
-                new Pair<>(31,6),
-                new Pair<>(19,7)
+        final var answers = List.of(
+                new Pair<>(7L, 0L),
+                new Pair<>(13L, 1L),
+                new Pair<>(59L, 4L),
+                new Pair<>(31L, 6L),
+                new Pair<>(19L, 7L)
         );
         IntStream.range(0, answers.size()).forEach(idx -> assertEquals(answers.get(idx), delays.get(idx)));
     }
@@ -48,14 +48,14 @@ class Day13Test {
         assertEquals(1202161486, Day13.findFirstMatchingTimestamp(delays("1789,37,47,1889")));
     }
 
-    List<Pair<Integer, Integer>> delays(String busses) {
-        return Day13.delays(Arrays.stream(busses.split(",")).collect(Collectors.toList()));
+    List<Pair<Long, Long>> delays(String busses) {
+        return Day13.delays(Arrays.stream(busses.split(",")).toList());
     }
 
     @Test
     void part2() {
-        final List<String> puzzleInput = ResourceLines.list(("/day13.txt"));
-        final List<String> busses = Arrays.stream(puzzleInput.get(1).split(",")).collect(Collectors.toList());
+        final var puzzleInput = ResourceLines.list(("/day13.txt"));
+        final var busses = Arrays.stream(puzzleInput.get(1).split(",")).collect(Collectors.toList());
         Day13.part2(busses);
     }
 }
